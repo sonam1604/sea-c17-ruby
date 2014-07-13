dfcf2467a62e0d7b2120a4254ccb9de82442e83a
Sonam Agarwal <sonam1604@gmail.com>
2014-07-13 15:09:04 -0700

    Merge Files

--- class2/1_hours_in_a_year.rb
+++ class2/1_hours_in_a_year.rb
@@ -1,5 +1,6 @@
 # Section 2.5 on page 12
 # 4 points
+#
 # How many hours are in a year?
 
 # For Non Leap Year
--- class2/2_minutes_in_a_decade.rb
+++ class2/2_minutes_in_a_decade.rb
@@ -1,5 +1,6 @@
 # Section 2.5 on page 12
 # 4 points
+#
 # How many minutes are in a decade?
  
 #In a decade we will have 2 - Leap Years and 8 - Non-Leap Years
